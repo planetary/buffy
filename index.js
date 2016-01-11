5f046855310c865412ce9036ec8eff940cbc2695
Joshua Gross <joshua@gross.is>
2016-01-11 13:14:24 -0500

    First complete pass at proactive Trello bot

---
+++ index.js
@@ -0,0 +1,227 @@
+'use strict';
+
+const bluebird = require('bluebird');
+const Botkit = require('botkit');
+const moment = require('moment');
+const path = require('path');
+const schedule = require('node-schedule');
+const Trello = require('node-trello');
+
+if(!process.env.token)
+    throw new Error('Specify token in environment');
+
+const t = new Trello(process.env.trello_key, process.env.trello_secret);
+bluebird.promisifyAll(t);
+
+const controller = Botkit.slackbot({
+    debug: process.env.debug,
+    'json_file_store': path.join(__dirname, 'data')
+});
+bluebird.promisifyAll(controller.storage.users);
+
+const bot = controller.spawn({
+    token: process.env.token
+}).startRTM();
+
+const checkUserTrello = () => {
+    bot.api.users.list({}, (err, res) => {
+        if(err || !res.ok) {
+            console.error(`Couldn't retrieve user list!`, err);
+        }
+
+        let teammates = [];
+        res.members.forEach((user) => {
+            if(!user.is_bot &&
+                !user.is_restricted &&
+                !user.ultra_restricted &&
+                !user.deleted &&
+                user.name !== 'slackbot' /* is_bot === false for slackbot. really? */) {
+                teammates.push(user);
+            }
+        });
+
+        if(process.env.debug) {
+            teammates = [
+                {
+                    id: 'U02ESHJRL',
+                    name: 'josh'
+                }
+                // },
+                // {
+                //     id: 'U02EQ5RCD',
+                //     name: 'matt'
+                // }
+            ];
+        }
+
+        teammates.forEach((user) => {
+            controller.storage.users.get(user.id, (err, data) => {
+                if(err)
+                    console.log(`Could not find user data for ${user.id} because:\n`, err);
+
+                if(data && data.trello)
+                    return;
+
+                console.log(`No trello data for ${user.name}`);
+
+                bot.startPrivateConversation({
+                    user: user.id
+                }, (err, convo) => {
+                    if(err) {
+                        console.error(
+                            `Couldn't send a Trello username request to ${user.name} because:\n`,
+                            err
+                        );
+                        return;
+                    }
+
+                    const checkUsername = (question) => {
+                        convo.ask(
+                            question || `Hey, real quick: what's your Trello username?`,
+                            [
+                                {
+                                    pattern: new RegExp(
+                                        '(^u[mh]+)|(hold on)|((one|a) sec(ond)?)|' +
+                                        '(\\bwait\\b)|(^ok)|(got it)|(^no|nope$)'
+                                    ),
+                                    callback: (response, convo) => {
+                                        convo.silentRepeat();
+                                    }
+                                },
+                                {
+                                    default: true,
+                                    callback: (response, convo) => {
+                                        convo.say(`Hold on, let me check that...`);
+                                        convo.next();
+
+                                        t.getAsync('/1/members/' + response.text + '/cards')
+                                        .then(() => {
+                                            convo.say('Great, thanks!');
+                                            controller.storage.users.save({
+                                                id: user.id,
+                                                trello: response.text
+                                            });
+                                            convo.next();
+                                        })
+                                        .catch(() => {
+                                            convo.next();
+                                            checkUsername(
+                                                `Hmm...I couldn't find that user. ` +
+                                                `Could you check again?`
+                                            );
+                                        });
+                                    }
+                                }
+                            ]
+                        );
+                    };
+
+                    checkUsername();
+                });
+            });
+        });
+    });
+};
+
+const lateTasks = (userId) => {
+    let _boards = {};
+
+    t.getAsync('/1/members/me/boards')
+    .then((boards) => {
+        let boardProps = {};
+        boards.forEach((board) => {
+            boardProps[board.id] = {
+                name: board.name,
+                lists: {}
+            };
+        });
+
+        return bluebird
+        .map(boards, (board) => {
+            return t.getAsync('/1/board/' + board.id + '/lists');
+        })
+        .then((lists) => {
+            lists.forEach((group, index) => {
+                group.forEach((list) => boardProps[boards[index].id].lists[list.id] = list.name);
+            });
+
+            _boards = boardProps;
+
+            if(userId)
+                return controller.storage.users.getAsync(userId)
+                    .then((user) => [user]); // to keep the out format like the `all` call below
+
+            return controller.storage.users.allAsync();
+        });
+    })
+    .then((users) => {
+        return users.map((user) => {
+            if(!user.trello)
+                return;
+
+            return t
+                .getAsync('/1/members/' + user.trello + '/cards')
+                .then((cards) => ({
+                    user: user,
+                    cards: cards
+                }));
+        });
+    })
+    .each((data) => {
+        let out = {};
+        let lateCards = 0;
+        Object.keys(_boards).forEach((board) => out[board] = []);
+        data.cards.forEach((card) => {
+            const list = _boards[card.idBoard].lists[card.idList].toLowerCase();
+            const due = moment(card.badges.due);
+
+            if(due.isBefore(new Date()) && !list.match(/completed|done|shipped/)) {
+                lateCards++;
+                out[card.idBoard].push(
+                    `${due.format('MMM D, YYYY @ h:mma')}: <${card.shortUrl}|${card.name}>`
+                );
+            }
+        });
+
+        if(!lateCards)
+            return;
+
+        const tasks = [];
+        Object.keys(out).forEach((board) => {
+            if(!out[board].length)
+                return;
+
+            tasks.push({
+                fallback: `${_boards[board].name}: ${out[board].length} tasks`,
+                title: _boards[board].name,
+                text: out[board].join('\n'),
+                color: '#838C91'
+            });
+        });
+
+        bot.startPrivateConversation({
+            user: data.user.id
+        }, (err, convo) => {
+            if(err) {
+                console.error(
+                    `Could not start conversation with user ${data.user.id} because:\n${err}`
+                );
+                return;
+            }
+
+            convo.say({
+                text:
+                    `Hey! A few of your tasks are recently past due. Could you go through ` +
+                    `and add comments or update the expected completion dates?\n\n`,
+                attachments: tasks
+            });
+        });
+    })
+    .catch((err) => console.trace(err));
+};
+
+schedule.scheduleJob('0 30 9 * * 1', checkUserTrello);
+schedule.scheduleJob('0 30 10 * * 1', lateTasks);
+
+controller.hears('late', ['direct_message'], (bot, message) => lateTasks(message.user));
+controller.hears('^check users$', ['direct_message'], checkUserTrello);
